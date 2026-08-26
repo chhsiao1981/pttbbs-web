f566ee9564629f9ffde845a8b51f98ad78b485b2
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-08-26 16:21:28 -0400

    chore: simplify package.json.
    use vite.config.ts for multi-page build.
    
    Signed-off-by: Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  input: {
+    all: resolve(import.meta.dirname, "htmls/all.html"),
+    home: resolve(import.meta.dirname, "htmls/home.html"),
+    login: resolve(import.meta.dirname, "htmls/login.html"),
+    register: resolve(import.meta.dirname, "htmls/register.html"),
+    "user-info": resolve(import.meta.dirname, "htmls/user-info.html"),
+    "change-email": resolve(import.meta.dirname, "htmls/change-email.html"),
+    "attempt-change-email": resolve(
+      import.meta.dirname,
+      "htmls/attempt-change-email.html",
+    ),
+    init: resolve(import.meta.dirname, "htmls/init.html"),
+    error: resolve(import.meta.dirname, "htmls/error.html"),
+  },
 });
